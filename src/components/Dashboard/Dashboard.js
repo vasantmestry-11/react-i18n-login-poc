6095564c6884a44cf806512fb2862c0a00b9c85b
Vasant Mestry <Vasant.Mestry@Xoriant.Com>
2023-03-20 10:52:25 +0530

    :zap: Added column translations for table columns on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,32 +1,41 @@
+import { useTranslation } from "react-i18next";
 import "./Dashboard.css";
 const Dashboard = () => {
+  const { t } = useTranslation();
+
+  const columns = [
+    { id: "id", label: t("id") },
+    { id: "name", label: t("name") },
+    { id: "location", label: t("location") },
+  ];
+
+  const row = [
+    { id: 1, name: "Vasant", location: "Mumbai" },
+    { id: 2, name: "Pavan", location: "Pune" },
+    { id: 3, name: "Aditya", location: "Banglore" },
+  ];
+
   return (
     <div className="container">
       <h1 className="text-white mb-5">Dashboard</h1>
       <table className="table table-dark table-hover text-white">
         <thead>
           <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Name</th>
-            <th scope="col">Location</th>
+            {columns.map(({ id, label }) => (
+              <th key={id}>{label}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Vasant</td>
-            <td>Mumbai</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Pavan</td>
-            <td>Pune</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Aditya</td>
-            <td>Banglore</td>
-          </tr>
+          {row.map(({ id, name, location }) => (
+            <tr>
+              <th key={id} scope="row">
+                {id}
+              </th>
+              <td>{name}</td>
+              <td>{location}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
